ee02b74d4116de003dd0b0a06819f7800c954925
Mathieu Gicquel-Bourdeau <mathieu.gicquel--bourdeau@isen-ouest.yncrea.fr>
2026-03-16 15:15:02 +0100

    consigne

--- consigne.c
+++ consigne.c
@@ -1,10 +1,31 @@
 #include "consigne.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-   float consigne(float thermostatPrec_f)
-   {
-   
+#define VERROU   "verrouconsigne.txt"
+#define CONSIGNE "consigne.txt"
+
+
+
+   float consigne(float thermostatPrec_f){
       float thermostat_f=0;
-   
+
+      FILE *Verrouconsigne = fopen(VERROU, "WX");
+      if(Verrouconsigne == NULL){
+         printf("Le fichier consigne est deja utiliser\n");
+      }close(Verrouconsigne);
+
+      FILE *consigne = fopen(CONSIGNE, "r");
+      if(consigne == NULL){
+         printf("erreur ouverture fichier\n");
+      }close(consigne);
+
+      if(consigne != NULL){
+         fscanf(consigne, "%f", &thermostat_f);
+      }
+
+      fclose(consigne);
+      remove(VERROU);
      
       return thermostat_f;
    
