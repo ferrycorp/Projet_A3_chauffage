270b601831259b55c11715b42bd318350dcc8531
ferrycorp <mathis.chartier.fr@gmail.com>
2026-03-16 12:30:35 +0100

    visualisation

--- visualisationT.c
+++ visualisationT.c
@@ -2,5 +2,14 @@
 
 void visualisationT(temp_t myTemp)
 {
-   
+    FILE* verrou = fopen("verrou_data.txt", "wx");
+    if (verrou == NULL) {
+        // Le fichier existe déjà, attendre qu'il soit libéré
+        return;
+    }
+    fclose(verrou);
+
+    
+
+    remove(".verrouData");
 }
