c48a50f56463b186f21e87fd2a20b3db240ae1c7
Mathymeur <152413790+Mathymeur@users.noreply.github.com>
2026-03-17 15:25:03 +0100

    define bieng

--- define.h
+++ define.h
@@ -1,13 +1,26 @@
 #ifndef DEFINE_H
 #define DEFINE_H
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+/********************************************************************/
+
+
+
+
+/********************************************************************/
 
 typedef struct {
 	float interieure;
 	float exterieure;
 }temp_t;
 
+typedef struct {
+	int initFlag;// drapeau initialisation 
+	float erreur;// precedente erreur
+	float integrale;// precedente integrale
+}prec_t;
 
 #endif
